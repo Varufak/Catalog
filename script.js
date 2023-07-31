47c4fcb3862eaed3bba6ade63a66b3bc26bf988a
Su-pr007 <maksimka.suvorov.01@list.ru>
2023-07-31 23:52:43 +0300

    dynamic js data added

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 let priceList = [
-    {name: 'Молоко', price: '79'},
-    {name: 'Яйца', price: '49'},
-    {name: 'Хлеб', price: '56'},
-    {name: 'Чебупели', price: '169'}
+    {id: '0', name: 'Молоко', price: '79'},
+    {id: '1', name: 'Яйца', price: '49'},
+    {id: '2', name: 'Хлеб', price: '56'},
+    {id: '3', name: 'Чебупели', price: '169'}
 ]
-
-let milk = priceList[0].name;
-let eggs = priceList[1].name;
-let bread = priceList[2].name;
-let chebupels = priceList[3].name;
-let milkPrice = priceList[0].price;
-let eggsPrice = priceList[1].price;
-let breadPrice = priceList[2].price;
-let chebupelsPrice = priceList[3].price;
-
 
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-    document.querySelector('#milk').innerHTML = milk;
-    document.querySelector('#eggs').innerHTML = eggs;
-    document.querySelector('#bread').innerHTML = bread;
-    document.querySelector('#chebupels').innerHTML = chebupels;
-    document.querySelector('#milkPrice').innerHTML = milkPrice;
-    document.querySelector('#eggsPrice').innerHTML = eggsPrice;
-    document.querySelector('#breadPrice').innerHTML = breadPrice;
-    document.querySelector('#chebupelsPrice').innerHTML = chebupelsPrice;
+    const tableElement = document.querySelector('#catalog');
+    for (productItem of priceList) {
+        // Создание ячеек
+        const nameCell = document.createElement('td');
+        const priceCell = document.createElement('td');
+        const buttonsCell = document.createElement('td');
+    
+        // Заполнение атрибутов ячеек
+        nameCell.className = 'product-name';
+        priceCell.className = 'product-price';
+        buttonsCell.className = 'product-buttons';
+        buttonsCell.align = 'center';
+
+        nameCell.innerHTML = productItem.name;
+        priceCell.innerHTML = productItem.price;
+
+    
+        // Кнопка добавления в корзину
+        const addToBasketButton = document.createElement('button');
+        addToBasketButton.dataset.id = productItem.id;
+        addToBasketButton.innerHTML = '+';
+        addToBasketButton.addEventListener('click', addToBasketHandler);
+        
+        
+        // Текст "Добавить в корзину"
+        const buttonText = document.createElement('div');
+        buttonText.innerHTML = 'Добавить в корзину';
+        
+        // Добавление кнопок в ячейки для кнопок
+        buttonsCell.append(addToBasketButton);
+        buttonsCell.append(buttonText);
+
+        // Создание строки
+        const newRow = document.createElement('tr');
+        newRow.append(nameCell);
+        newRow.append(priceCell);
+        newRow.append(buttonsCell);
+
+        tableElement.append(newRow);
+    }
 }
 
+function addToBasketHandler(event) {
+    const button = event.target;
+    const itemId = button.dataset.id;
+}
 
-document.querySelector('#milkButton').addEventListener('click', addMilk);
-document.querySelector('#eggsButton').addEventListener('click', addEggs);
-document.querySelector('#breadButton').addEventListener('click', addBread);
-document.querySelector('#chebupelsButton').addEventListener('click', addChebupels);
